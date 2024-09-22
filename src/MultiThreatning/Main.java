a0362f98d18d00e2be7585421b87e51c9d1fafb5
Artem Kokobelyan <artyomkokobelyanwork@gmail.com>
2024-09-22 20:05:59 +0400

    threats

--- src/MultiThreatning/Main.java
+++ src/MultiThreatning/Main.java
@@ -1,13 +1,48 @@
 package MultiThreatning;
 
+import java.util.Timer;
+
 public class Main {
 
+    static boolean isFive = false;
+
     public static void main(String[] args) {
-        Timer timer = new Timer();
-        Thread thread = new Thread(timer);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10_000_000; i++) {
+                    if (i == 5) {
+                        isFive = true;
+                    }
+                    System.out.println(i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
         thread.start();
-        for (int i = 0; i < 10_000_000; i++) {
-            System.out.println(2);
-        }
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10_000_000; i++) {
+                    if (isFive) {
+                        break;
+                    }
+                    System.out.println(i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        thread2.start();
+
     }
 }
